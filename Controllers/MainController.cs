26aff17f57645aa78b19a36bfe4209ea2a604208
Artem <artem.egorov.art2006@gmail.com>
2023-07-27 16:37:14 +0300

    Настроил получение котиков из Обалака для контроллера

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,3 +1,5 @@
+using Amazon.S3;
+using Amazon.S3.Model;
 using Deployf.Botf;
 using Hangfire;
 using MongoDB.Bson;
@@ -10,11 +12,24 @@ namespace CotikBotik.Controllers
     public class MainController : BotController
     {
         private readonly IMongoDatabase _dbClient;
+        private readonly IConfigurationRoot config;
+        private readonly AmazonS3Config configsS3;
 
         [Obsolete]
         public MainController(IMongoDatabase dbClient)
         {
             this._dbClient = dbClient;
+
+            var conf_builder = new ConfigurationBuilder();
+
+            conf_builder.SetBasePath(Directory.GetCurrentDirectory());
+            conf_builder.AddJsonFile("security.json");
+            config = conf_builder.Build();
+
+            configsS3 = new  AmazonS3Config() {
+                ServiceURL="https://storage.yandexcloud.net"
+            };
+
             RecurringJob.AddOrUpdate<RecurringPhotoJob>(x => x.Exec(), Cron.MinuteInterval(1));
 
         }
@@ -22,36 +37,46 @@ namespace CotikBotik.Controllers
         [Action("/start","Начало работы с ботом")]
         public async void Start()
         {
+            
             try
             {
-                PushL("Котики вперёд!");
-
-                Random rnd = new Random();
-                var files = Directory.GetFiles("C://Users//Artem//Desktop//Котики");
-                var photo = files[rnd.NextInt64(files.Length)].Replace("\\","//");
 
                 User userNow = new User() {login = Context.GetUsername(), chatId = this.ChatId};
 
                 var user = await _dbClient.GetCollection<User>("users").Find(x => x.login == userNow.login).FirstOrDefaultAsync();
 
-
                 if(user == null)
                 {
                     await _dbClient.GetCollection<User>("users").InsertOneAsync(userNow);
                 }
-
-                using (FileStream stream = System.IO.File.OpenRead(photo))
+                using (var client = new AmazonS3Client(config.GetSection("accessKey").Value, config.GetSection("secretKey").Value, configsS3))
                 {
-                    InputOnlineFile inputOnlineFile = new InputOnlineFile(stream, "Котик");
-                    await ((TelegramBotClient)Client).SendPhotoAsync(this.ChatId, inputOnlineFile);
-                }
+                    var requestKeys = new ListObjectsV2Request
+                    {
+                        BucketName = config.GetSection("bucketName").Value
+                    };
 
-                Send();
+                    ListObjectsV2Response response = await client.ListObjectsV2Async(requestKeys);
+
+                    Random rnd = new Random();
+                    var keyToPhoto = response.S3Objects[(int)rnd.NextInt64(10L)].Key;
+
+                    var requestPhoto = new GetObjectRequest();
+
+                    var responsePhoto = await client.GetObjectAsync(config.GetSection("bucketName").Value, keyToPhoto);
+
+                    var stream = responsePhoto.ResponseStream;
+                    
+                    InputOnlineFile inputOnlineFile = new InputOnlineFile(stream, "Котитки вперёд!");
+                    await ((TelegramBotClient)this.Client).SendPhotoAsync(userNow.chatId, inputOnlineFile, caption : "Коитики вперёд!");
+
+                    await stream.DisposeAsync();
+                }
             }
             catch (System.Exception ex)
             {
-                PushL(ex.Message );
-                Send();
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
             }
         }
 
